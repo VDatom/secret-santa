3ef8f7657152b66a5cc839f9fc1130193c04a2dd
Vijay Dev <115635229+VDatom@users.noreply.github.com>
2023-12-22 17:49:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function myFunction() {
   let value = prompt(" Hi, Enter Your Secret Time :- ");
   const data =(hour+":"+minutes);
   if(value == data) {
-    document.getElementById("demo").innerHTML = value+" ಸರಿಯಾದ ಉತ್ತರ, My Name Ends with Letter 'A' .";
+    document.getElementById("demo").innerHTML = value+" ಸರಿಯಾದ ಉತ್ತರ, I am not a boy and my name ends with Letter 'A' .";
     alert(" OHH! Got it... Press 'Enter' or 'Ok' to see your santa");
     setTimeout(function(){
       window.location.reload();
@@ -19,6 +19,6 @@ else{
 alert('HA HA HA, "Sorry Bro...Better Luck Next Time" Try Again');
 setTimeout(function(){
   window.location.reload();
-}, 500);
+}, 200);
 }
 }
